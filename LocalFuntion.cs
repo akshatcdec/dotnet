333bcdb2687521e05ffa94c2d92e8738142b4f88
akshatcdec <121384204+akshatcdec@users.noreply.github.com>
2023-07-04 17:51:12 +0530

    Add files via upload

---
+++ LocalFuntion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session3
+{
+    internal class LocalFuntion
+    {
+        public static void CalculateFee()
+        {
+
+            void IdCardFee()
+            {
+                Console.WriteLine("ID FEES");
+
+            }
+            void AdmFee()
+            {
+                Console.WriteLine("ADM FEES");
+
+            }
+            void TutionFee()
+            {
+                Console.WriteLine("Tution FEES");
+
+            }
+            Console.WriteLine("This Is caculateFee Logic");
+            IdCardFee();
+            AdmFee();
+            TutionFee();
+        }
+    }
+}
