030d2282ac7467262bccbf0a9cbad9c357e30465
akshatcdec <121384204+akshatcdec@users.noreply.github.com>
2023-07-04 18:03:36 +0530

    ReadOnly

---
+++ Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session3
+{
+    internal class Circle
+    {
+        public readonly double _radius;
+
+        public Circle(double radius)
+        { this._radius = radius; }
+        public double Radius { get { return _radius;} }
+
+    }
+}
