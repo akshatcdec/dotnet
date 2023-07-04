4af4be491a1711f06f3be56a0b29d7db994e3491
akshatcdec <121384204+akshatcdec@users.noreply.github.com>
2023-07-04 19:34:20 +0530

    Add files via upload

---
+++ Cal.cs
@@ -0,0 +1,22 @@
+﻿namespace StaticDemo
+{
+    public static class Cal
+    {
+       public static int Add(int x,int y)
+        {
+            return x + y;
+        }
+        public static int Div(int x, int y)
+        {
+            return x / y;
+        }
+        public static int Multi(int x, int y)
+        {
+            return x * y;
+        }
+        public static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
