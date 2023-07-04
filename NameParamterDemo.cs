a63bc9b6767e47605c7457dc69b137fa635502b8
akshatcdec <121384204+akshatcdec@users.noreply.github.com>
2023-07-04 17:39:57 +0530

    Add files via upload

---
+++ NameParamterDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session3
+{
+    internal class NameParamterDemo
+    {
+        public static void TestMethod(int x, int y, bool z)
+        {
+           Console.WriteLine("The value of x: "+x); 
+           Console.WriteLine("The value of Y: " + y);
+           Console.WriteLine("The value of Z: " + z);
+        }
+    }
+}
---
+++ ParamDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session3
+{
+    internal class ParamDemo
+    {
+        public static void PrintStudentName(params string[] names)
+        {
+            if (names.Length == 0)
+                Console.WriteLine("No Record");
+            else { 
+             foreach (string name in names)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
