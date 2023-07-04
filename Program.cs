166445c73fcf5ca8035bd37c912da0a7c05311ee
akshatcdec <121384204+akshatcdec@users.noreply.github.com>
2023-07-04 18:05:09 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(20);
+            circle._radius = 20;
+            // NameParamterDemo.TestMethod(10, 20, true);//Positional Parameter
+            //NameParamterDemo.TestMethod(z: false, x: 20, y: 30);//Named Parameter
+            // string[] name= { "A1","RAM","SHyam","SALMAN" };
+            //ParamDemo.PrintStudentName(name);
+            //LocalFuntion.CalculateFee();
+            Console.ReadKey();
+
+        }
+    }
+}
