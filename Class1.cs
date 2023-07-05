651807648d3940f4a55493ac972b298e17410aa7
akshatcdec <121384204+akshatcdec@users.noreply.github.com>
2023-07-05 08:56:02 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session5
+{
+    public class BaseClass
+    {
+        public void PrintMessage()
+        {
+            Console.WriteLine("This is base class Message");
+        }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public new void PrintMessage()
+        {
+            Console.WriteLine("This is Child class Message");
+        }
+    }
+}
---
+++ SealedMethodDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session5
+{
+    public class BaseClass
+    {
+        public virtual void PrintMessage()
+        {
+            Console.WriteLine("This is Base Class");
+        }
+    }
+
+    public class DerivedClass: BaseClass
+    {
+        public  override void PrintMessage()
+        {
+            Console.WriteLine("This is DerivedClass");
+        }
+    }
+    public class FurtherDerivedClass:DerivedClass
+    {
+        public override void PrintMessage()
+        {
+            Console.WriteLine("This is Further Derived Class");
+        }
+    }
+}
